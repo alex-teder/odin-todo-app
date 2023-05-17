54baf566ac4e4cbd6af4d15105f70c3d70f21878
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-05-17 23:03:22 +0400

    added Task, Project and filesystem

--- src/index.js
+++ src/index.js
@@ -30,3 +30,78 @@ function toggleTheme() {
 }
 
 document.querySelector("#theme-toggle").addEventListener("click", toggleTheme);
+
+class Task {
+  constructor(text) {
+    this.text = text;
+    this.isDone = false;
+  }
+
+  toggle() {
+    this.isDone = !this.isDone;
+  }
+}
+
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
+
+  changeName(newName) {
+    this.name = newName;
+  }
+
+  addTask(text) {
+    const t = new Task(text);
+    this.tasks.push(t);
+  }
+
+  deleteTask(index) {
+    this.tasks.splice(index, 1);
+  }
+
+  get tasksUnfinished() {
+    let result = 0;
+    for (let task of this.tasks) {
+      if (task.isDone === false) {
+        result++;
+      }
+    }
+    return result;
+  }
+
+  get percentComplete() {
+    let numberOfTasksDone = 0;
+    for (let task of this.tasks) {
+      if (task.isDone === true) {
+        numberOfTasksDone++;
+      }
+    }
+    return Math.round((numberOfTasksDone / this.tasks.length) * 100);
+  }
+}
+
+const filesystem = {
+  projects: [],
+  logbook: {
+    projects: [],
+    deleteProject: function (index) {
+      this.projects.splice(index, 1);
+    },
+  },
+  createProject: function (name) {
+    const p = new Project(name);
+    this.projects.push(p);
+  },
+  deleteProject: function (index) {
+    this.projects.splice(index, 1);
+  },
+  renameProject: function (index, newName) {
+    this.projects[index].changeName(newName);
+  },
+  moveToLogbook: function (index) {
+    const p = this.projects.splice(index, 1)[0];
+    this.logbook.projects.push(p);
+  },
+};
